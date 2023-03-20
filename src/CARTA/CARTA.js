c237e34dbace35d8686e12dceecaa640628bacdf
Rociliu <mrrosa.carrasco@gmail.com>
2023-03-20 20:56:47 +0100

    Creado archivo js con el objeto de la carta

---
+++ src/CARTA/CARTA.js
@@ -0,0 +1,138 @@
+export const CARTA = {
+    pizzas: [
+        {
+            name: "MARGHERITA",
+            ingredients: "Base de tomate, mozzarella y hojas frescas de albahaca",
+            price:"11,00 €",
+
+        },
+        {
+            name:"REGINA", 
+            ingredients: "Base de tomate, mozzarella, jamón di parma, parmesano, rúcula y tomate cherry",
+            price: "14,50 €",
+
+        },
+        {
+            name:"DIAVOLA", 
+            ingredients: "Base de tomate, mozzarella, salame picante, nduja di calabria, peperonciono dolce y parmesano",
+            price: "13,50 €",
+
+        },
+        {
+            name:"FORMAGGIO", 
+            ingredients: "Gorgonzola, mozzarella, parmigiano, ricotta",
+            price: "14,50 €",
+
+        },
+        {
+            name:"CALZONE", 
+            ingredients: "Base cerrada rellena de salami picante, tomate, aceitunas y ricotta",
+            price: "15,00 €",
+
+        }
+    ],
+    pastas: [
+        {
+            name:"SPAGHETTI FRUTTI DI MARE", 
+            ingredients: "Spaghetti, gambas rojas, calamar, mejillón y salsa de tomate",
+            price: "16,50 €",
+
+        },
+        {
+            name:"BUCATINI ALLA AMATRICIANA", 
+            ingredients: "Pasta típica de Roma con guanciale, salsa de tomate y pecorino",
+            price: "13,00 €",
+
+        },
+        {
+            name:"LASAGNA DE BOLOGNESA", 
+            ingredients: "Bechamel, carne picada de cerdo y ternera, salsa de tomate confitado y queso parmigiano",
+            price: "13,90 €",
+
+        },
+        {
+            name:"RAVIOLI DI SCAMPI ", 
+            ingredients: "Raviolis rellenos de cigalas con salsa de mantequilla, tomate, tomillo, limón, albahaca fresca y ralladura de lima",
+            price: "16,00 €",
+
+        },
+        {
+            name:"MAFALDINE AL TARTUFO", 
+            ingredients: "Pasta mafaldine con setas y crema de nata y trufa",
+            price: "16,50 €",
+
+        },
+    ],
+    postres: [
+        {
+            name:"PIZZETA DI NUTELLA",
+            ingredients:"Pizza con nutella y helado de galletas Lotus",
+            price:"10,00 €",
+
+        },
+        {
+            name:"AMARENA",
+            ingredients:"Copa de nata aromarizada a la vainilla, helado de frambuesa, cerezas amarenas, almendras y galletas Amaretti",
+            price:"6,00 €",
+
+        },
+        {
+            name:"TTIRAMISÚ",
+            ingredients:"Bizcotelas bañadas en Baileys, cacao y crema de mascarpone",
+            price:"6,00 €",
+
+        },
+        {
+            name:"BACCIO",
+            ingredients:"Tarta de queso horneada con dulce de leche",
+            price:"8,00 €",
+
+        },
+
+    ],
+    bebidas: [
+        {
+            name: "AQUA SAN PELLEGRINO",
+            price: "2,50 €",
+        },
+        {
+            name: "REFRESCOS Y ZUMOS",
+            price: "2,50 €",
+        },
+        {
+            name: "MARTINI BIANCO",
+            price: "5,00 €",
+        },
+        {
+            name: "MARTINI ROSSO",
+            price: "5,00 €",
+        },
+        {
+            name: "BIRRA MORETTI",
+            price: "3,00 €",
+        },
+        
+        {
+            name: "GRAPPA",
+            price: "4,00 €",
+        },
+        {
+            name: "LIMONCELLO",
+            price: "4,00 €",
+        },
+        {
+            name: "SPRESSO",
+            price: "2,00 €",
+        },
+        {
+            name: "CAPUCCINO",
+            price: "2,50 €",
+        },
+        
+
+    ],   
+
+}
+   
+
+
