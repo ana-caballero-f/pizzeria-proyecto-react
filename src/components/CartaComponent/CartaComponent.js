7f522f58868bea442a994affc140dc27da095176
Rociliu <mrrosa.carrasco@gmail.com>
2023-03-20 21:01:49 +0100

    Importado y mapeado el objeto CARTA a CartaComponent

--- src/components/CartaComponent/CartaComponent.js
+++ src/components/CartaComponent/CartaComponent.js
@@ -2,10 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './CartaComponent.module.scss';
 
+import { CARTA } from '../../CARTA/CARTA';
+
+const { pizzas, pastas, postres, bebidas } = CARTA;
 
 const CartaComponent = () => (
   <div className={styles.CartaComponent}>
-    CartaComponent Component
+  <h1>hola ana</h1>
+
+    <div className={styles.TitleCarta}>
+      <h2>PIZZA FATTA A MANO </h2>
+      <p>Si te gustan las pizzas, las nuestras te enamorarán</p>
+    </div>
+
+    <div className={styles.ContainerCarta}>
+      {pizzas.map((item)=>{
+        return(
+          <div key={JSON.stringify(item)}>
+            <p className={styles.Name}>{item.name}</p>
+            <p className={styles.Ingredients}>{item.ingredients}</p>
+            <p className={styles.Price}>{item.price}</p>
+          </div>
+        );
+      })}
+
+    </div>
+
+
+
+    
   </div>
 );
 
