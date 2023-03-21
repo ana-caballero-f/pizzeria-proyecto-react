249113d34b098de8297301fe2d19b6ea94e6a254
ana caballero <anacaballerof@hotmail.com>
2023-03-21 08:57:35 +0100

    cambios en home

--- src/components/LogoComponent/LogoComponent.js
+++ src/components/LogoComponent/LogoComponent.js
@@ -2,9 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './LogoComponent.module.scss';
 
+import ImagenLogo from "../../assets/images/logo.jpg"
+
 const LogoComponent = () => (
   <div className={styles.LogoComponent}>
     LogoComponent Component
+    {/* <img src={ImagenLogo}></img> */}
   </div>
 );
 
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,11 +1,17 @@
+
 import styles from './HomePage.module.scss';
 
+
+import HomeComponent from '../../components/HomeComponent/HomeComponent';
+import PizzaFoto from "../../assets/images/pizza.jpg";
 
 export default function HomePage () {
 
     return (
         <div className={styles.HomePage}>
-            <h1>home page</h1>
+        <h1>Home</h1>
+            <img className={styles.Image} src={PizzaFoto} alt="foto pizza"></img>
+            <HomeComponent></HomeComponent>
         </div>
     )
 }
